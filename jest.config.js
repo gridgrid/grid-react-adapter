ff50e18d392f607cafc7e791fdbb8093aac7e13f
Sterling Camden <rusty.camden@gmail.com>
2017-08-30 15:05:15 -0700

    squashed base merge

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const transformTSPaths = require('./transformTSPaths');
+const jestTransforms = require('transform-ts-paths').jestTransforms;
+const moduleNameMapper = Object.assign({}, transformTSPaths(
+  jestTransforms.alias,
+  jestTransforms.path
+));
+
+module.exports = {
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.(ts|tsx)': '<rootDir>/node_modules/ts-jest/preprocessor.js'
+  },
+  verbose: true,
+  testEnvironment: 'node',
+  testRegex: 'src/((.+/)?__tests__/.*|.*\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js'
+  ],
+  moduleNameMapper: moduleNameMapper,
+  // watchPathIgnorePatterns: ['dist']
+};
