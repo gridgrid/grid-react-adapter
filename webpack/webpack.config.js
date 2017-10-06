66d7c7857f2b65220d6e2660d2564f3b7da6dc69
Sterling Camden <rusty.camden@gmail.com>
2017-10-06 15:40:28 -0700

    fix(webpack): this source mpa loader, realllllly needs to be a preloader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -202,6 +202,7 @@ export default (opts) => {
       },
       {
         test: /\.js$/,
+        enforce: 'pre',
         use: ["source-map-loader"]
       },
       // this is just for libraries that are imported ATL doesn't transform those
