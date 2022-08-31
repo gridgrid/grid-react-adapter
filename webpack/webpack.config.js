0f3766bf8cddc3ac912714ff1d21497dcc55c790
Sterling Camden <rusty.camden@gmail.com>
2022-08-31 09:42:12 -0700

    build(webpack): upgrade to webpack 5 included loaders and plugins and node-sass

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,14 +1,11 @@
 const webpack = require('webpack');
-const WebpackMd5Hash = require('webpack-md5-hash');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 const  path = require('path');
 
 
 const transformTsConfigPaths = require('../transformTSPaths');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const globalSassRegex = /(toastr|grid)\.scss$/;
 const aliases = transformTsConfigPaths();
 
@@ -22,6 +19,7 @@ module.exports = (opts) => {
   const isLibrary = opts.isLibrary || false;
 
   const useStagingUrls = !!isStagingApi || (!isDev && branchName !== 'prod');
+  const isStagingOrProd = branchName === 'prod' || branchName === 'staging' || branchName ==='master' || branchName === 'main';
   const entry = [
     ...(isDev && ['./src/app/webpack-public-path', 'webpack-hot-middleware/client?reload=true'] || []),
     isLibrary ? './src/lib/index' : './src/app/index'
@@ -51,13 +49,6 @@ module.exports = (opts) => {
       };
 
   const plugins = [
-    // isDev && new BundleAnalyzerPlugin() ||  || (() => {}),
-
-    ...(!isDev && !isLibrary && [
-      // Hash the files using MD5 so that their names change when the content changes.
-      new WebpackMd5Hash(),
-
-    ] || []),
 
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(isDev ? 'production' : 'production'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
@@ -99,11 +90,6 @@ module.exports = (opts) => {
       __OFFLINE_MODE__: !!(isDev && isOffline)
     }),
 
-    ...(isDev && [
-      new webpack.HotModuleReplacementPlugin(),
-      // new webpack.NoErrorsPlugin(),
-    ] || []),
-
     new MiniCssExtractPlugin({
       filename: isDev ? 'app.css' : `[name]${!isLibrary && '.[contenthash]' || ''}.css`,
     }),
@@ -125,54 +111,7 @@ module.exports = (opts) => {
     }),
   ];
 
-  const extractTextOptionsNonGlobal = [
-    {
-      loader: MiniCssExtractPlugin.loader,
-      options: {
-        esModule: true,
-        modules: {
-          namedExport: true,
-        },
-      },
-    },
-    {
-      loader: 'typings-for-css-modules-loader',
-      options: {
-        namedExport: true,
-        camelCase: true,
-        sourceMap: true,
-        modules: true,
-        importLoaders: 1,
-        localIdentName: '[name]__[local]___[hash:base64:5]'
-      }
-    },
-    {
-      loader: 'postcss-loader',
-      options: {
-        plugins: (loader) => [
-          autoprefixer(),
-        ],
-        sourceMap: true
-      }
-    },
-      'resolve-url-loader?sourceMap',
-      'sass-loader?sourceMap'
-    ]
-  ;
-
-  // yaya it's dirty but it's also DRY. DRY and dirty suckas.
-  const extractTextOptionsGlobal = JSON.parse(JSON.stringify(extractTextOptionsNonGlobal));
-  const extractTextOptionsCss = JSON.parse(JSON.stringify(extractTextOptionsNonGlobal));
-  extractTextOptionsCss[0] = extractTextOptionsGlobal[0] = {
-    loader: 'css-loader',
-    options: {
-      modules: false,
-      sourceMap: true,
-      importLoaders: 1,
-      localIdentName: '[name]__[local]___[hash:base64:5]'
-    }
-  };
-  extractTextOptionsCss.splice(extractTextOptionsCss.indexOf('sass-loader'), 1);
+  const extractCSS = true;
 
   const module = {
     rules: [{
@@ -189,77 +128,74 @@ module.exports = (opts) => {
       use: ["source-map-loader"]
     },
     {
-      test: /\.eot(\?v=\d+.\d+.\d+)?$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          'name': 'assets/fonts/[name].[ext]'
-        }
-      }
+      test: /\.(eot|woff2?|ttf|otf|svg)(\?v=\d+.\d+.\d+)?$/,
+      type: 'asset',
+      generator: {
+        filename: 'assets/fonts/[path][name][ext]',
+      },
     },
     {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/font-woff',
-          name: 'assets/fonts/[name].[ext]'
-        }
-      }]
+      test: /\.(jpe?g|png|gif|pdf|ico)$/i,
+      type: 'asset/resource',
+      generator: {
+        filename: 'assets/images/[name][ext]',
+      },
     },
     {
-      test: /\.ttf(\?v=\d+.\d+.\d+)?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'application/octet-stream',
-          name: 'assets/fonts/[name].[ext]'
-        }
-      }]
+      test: /\.s?css$/,
+      use: [
+        !extractCSS
+          ? {
+              loader: 'style-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  namedExport: true,
+                },
+              },
+            }
+          : {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                esModule: true,
+              },
+            },
+        {
+          loader: 'css-loader',
+          options: {
+            esModule: true,
+            modules: {
+              namedExport: true,
+              auto: true,
+              localIdentName: isStagingOrProd ? '[hash:base64]' : '[path][name]__[local]',
+            },
+            sourceMap: false,
+            importLoaders: 1,
+          },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            options: {
+              plugins: () => [autoprefixer()],
+              sourceMap: false,
+            },
+          },
+        },
+        {
+          loader: 'resolve-url-loader',
+          options: {
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        },
+      ],
     },
-    {
-      test: /\.svg(\?v=\d+.\d+.\d+)?$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          mimetype: 'image/svg+xml',
-          name: 'assets/fonts/[name].[ext]'
-        }
-      }],
-    },
-    {
-      test: /\.(jpe?g|png|gif|pdf)$/i,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: 'assets/images/[name].[ext]'
-        }
-      }]
-    },
-    {
-      test: /\.ico$/,
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: 'assets/icons/[name].[ext]'
-        }
-      }]
-    },
-    {
-      test: (absPath) => /\.scss$/.test(absPath) && !globalSassRegex.test(absPath),
-      use:extractTextOptionsNonGlobal
-    },
-    {
-      test: globalSassRegex,
-      use:extractTextOptionsGlobal
-    },
-    {
-      test: /\.css$/,
-      use:extractTextOptionsCss
-    }
     ]
   };
 
